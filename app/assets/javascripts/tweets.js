38d76f72ce7b7d6ff521232dc513543608bb2460
DocPrior <Sara Prior>
2017-07-12 15:42:58 -0400

    finish part 1

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$('#new_tweet').on('submit', function(e) {
+  e.preventDefault();
+  $.ajax({
+    url: $(this).attr('action'),
+    method: $(this).attr('method'),
+    data: $(this).serialize(),
+    dataType: 'html'
+  }).done(function(data) {
+    $('.tweets').prepend(data);
+  })
+})
